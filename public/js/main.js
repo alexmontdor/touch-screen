83e1480f7ea15fc1dc4648c72c9bb6df430af3e7
Alexandre LANDREAU <alexandre@landreau.biz>
2018-01-10 23:39:47 +0100

    start the move start

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,24 @@
+var offsetX, offsetY
 var element = document.getElementById('drag-bozo');
 
-// Move Bozzo
 
+// Start Dragging bozo
+element.addEventListener('touchstart', function(event) {
+  var touch = event.targetTouches[0];
+  
+  offsetX= element.pageX - touch.pageX
+  offsetY= element.pageY - touch.pagey
+},false)
+
+// Move Bozo
 element.addEventListener('touchmove', function(event) {
   var touch = event.targetTouches[0];
-  element.style.left = touch.pageX + 'px';
-  element.style.top = touch.pageY + 'px';
+  element.style.left = touch.pageX + offsetX + 'px';
+  element.style.top = touch.pageY + + offsetY + 'px';
   event.preventDefault();
 }, false);
 
 
-// drop Bozzo
+
+
+// drop Bozo
