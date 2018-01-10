a1bdf254e585db8456ca9faf4a75fb0cf16dbbaa
Alexandre LANDREAU <alexandre@landreau.biz>
2018-01-10 23:19:53 +0100

    create Javascript

---
+++ public/js/main.js
@@ -0,0 +1,15 @@
+var element = document.getElementById('drag-bozzo');
+
+
+
+// Move Bozzo
+
+element.addEventListener('touchmove', function(event) {
+  var touch = event.targetTouches[0];
+  element.style.left = touch.pageX + 'px';
+  element.style.top = touch.pageY + 'px';
+  event.preventDefault();
+}, false);
+
+
+// drop Bozzo
