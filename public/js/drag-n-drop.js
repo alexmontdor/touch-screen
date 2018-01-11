6b5a588244a5a0fe646806fdec1687bfdb54bb28
Alexandre LANDREAU <alexandre@landreau.biz>
2018-01-11 13:00:05 +0100

    NOT WORKING

--- public/js/drag-n-drop.js
+++ public/js/drag-n-drop.js
@@ -5,6 +5,10 @@ var dropZones = document.getElementsByClassName('droppable');
 element.addEventListener('dragstart', handleDragStart, false)
 element.addEventListener('dragend', handleDragEnd, false)
 
+for (var i = 0; i < dropZones.length; i++) {
+  dropZones[i].addEventListener('drop', handleDrop);
+}
+
 /**
  * touchstart -> dragstart
  * touchmove -> drag
@@ -14,97 +18,69 @@ element.addEventListener('dragend', handleDragEnd, false)
  */
 
 // Start Dragging bozo
-element.addEventListener('touchstart', function(event) {
+element.addEventListener('touchstart', function (event) {
   var touch = event.targetTouches[0];
-  
-  offsetX= element.offsetLeft - touch.pageX;
-  offsetY= element.offsetTop - touch.pageY;
+
+  offsetX = element.offsetLeft - touch.pageX;
+  offsetY = element.offsetTop - touch.pageY;
 
 }, false);
 
 // Move Bozo
-element.addEventListener('touchmove', function(event) {
+element.addEventListener('touchmove', function (event) {
   var touch = event.targetTouches[0];
-  
+
   event.preventDefault();
   element.style.left = touch.pageX + offsetX + 'px';
   element.style.top = touch.pageY + offsetY + 'px';
 }, false);
 
 
-    var dragSrcEl = null;
-    function handleDragStart(e) {
-      dragSrcEl = e.target;
-      var dt = e.dataTransfer;
-      dt.effectAllowed = 'move';
-      dt.setData('text', 'Bozs dragged');
+var dragSrcEl = null;
+function handleDragStart(e) {
+  dragSrcEl = e.target;
+  var dt = e.dataTransfer;
+  dt.effectAllowed = 'move';
+  dt.setData('text', 'Bozs dragged');
 
-    }
-    function handleDragOver(e) {
-        if (dragSrcEl) {
-            e.preventDefault();
-            e.dataTransfer.dropEffect = 'move';
-            
-        }
-    }
-    function handleDragEnter(e) {
-        if (dragSrcEl) {
-            e.target.classList.add('over');
-        }
-    }
-    function handleDragLeave(e) {
-        if (dragSrcEl) {
-            e.target.classList.remove('over');
-        }
-    }
-    function handleDragEnd(e) {
-        dragSrcEl = null;
-        /* [].forEach.call(cols, function (col) {
-            col.style.opacity = '';
-            col.classList.remove('over');
-        }); */
-    }
-    function handleDrop(e) {
-        if (dragSrcEl) {
-            e.stopPropagation();
-            e.stopImmediatePropagation();
-            e.preventDefault();
-            if (dragSrcEl != this) {
-              this.innerHTML = e.dataTransfer.getData('text');
-            }
-        }
 }
+function handleDragOver(e) {
+  if (dragSrcEl) {
+    e.preventDefault();
+    e.dataTransfer.dropEffect = 'move';
 
-
-function dragStartElement (event) {
-//  alert ('start dragging element');
-}
-
-function dragElement (event) {
-  event.preventDefault();
-  var target;
- // alert('dragging element');
-
-   
-  if (event.target.target) {
-    target = touch.target;
-    target.style.background = "purple";
   }
-  else {
-    if (target)
-    {
-      target.style.background = "";
-      target = null;
+}
+function handleDragEnter(e) {
+  if (dragSrcEl) {
+    e.target.classList.add('over');
+  }
+}
+function handleDragLeave(e) {
+  if (dragSrcEl) {
+    e.target.classList.remove('over');
+  }
+}
+function handleDragEnd(e) {
+  dragSrcEl = null;
+  /* [].forEach.call(cols, function (col) {
+      col.style.opacity = '';
+      col.classList.remove('over');
+  }); */
+}
+function handleDrop(e) {
+  if (dragSrcEl) {
+    e.stopPropagation();
+    e.stopImmediatePropagation();
+    e.preventDefault();
+    if (dragSrcEl != this) {
+      this.innerHTML = e.dataTransfer.getData('text');
     }
   }
-
 }
 
-function dropElement (event) {
+function dropElement(event) {
   event.preventDefault();
   alert('dropping element');
 }
 
-for (var i= 0; i < dropZones.length; i++){
-  dropZones[i].addEventListener('drop', handleDrop);
-}
