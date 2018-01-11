feb924f8e43bc95bba69c3e58937a26fef9b0154
Alexandre LANDREAU <alexandre@landreau.biz>
2018-01-11 23:25:39 +0100

    Drop for normal screen

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # touch-screen
 Small code to be used on touch screen
 
+# WORK IN PROGRESS
+- Setup a normal screen drag n Drop https://openclassrooms.com/courses/dynamisez-vos-sites-web-avec-javascript/le-drag-drop-2 ??
+- Use a polyfill to get the Drap n Drop working for touch screen (https://github.com/Bernardo-Castilho/dragdroptouch)
+- possibly "forking" from this polyfill
 
 # pre-requirement
 software required:
--- public/js/drag-n-drop.js
+++ public/js/drag-n-drop.js
@@ -2,85 +2,32 @@ var offsetX, offsetY;
 var element = document.getElementById('drag-bozo');
 var dropZones = document.getElementsByClassName('droppable');
 
-element.addEventListener('dragstart', handleDragStart, false)
-element.addEventListener('dragend', handleDragEnd, false)
+element.addEventListener('dragstart', handleDragStart, false);
 
-for (var i = 0; i < dropZones.length; i++) {
+for (var i=0; i<dropZones.length; i++) {
+  dropZones[i].addEventListener('dragenter', handleDragEnter);
+  dropZones[i].addEventListener('dragleave', handleDragLeave);
   dropZones[i].addEventListener('drop', handleDrop);
 }
 
-/**
- * touchstart -> dragstart
- * touchmove -> drag
- * touchend -> dragend
- * 
- * if (drag) test drop
- */
 
-// Start Dragging bozo
-element.addEventListener('touchstart', function (event) {
-  var touch = event.targetTouches[0];
+function handleDragStart (event) {
+  event.dataTransfert ('text/plain', 'Bozo is moved');        // compulsory with FireFox : text/plain
+}
 
-  offsetX = element.offsetLeft - touch.pageX;
-  offsetY = element.offsetTop - touch.pageY;
-
-}, false);
-
-// Move Bozo
-element.addEventListener('touchmove', function (event) {
-  var touch = event.targetTouches[0];
-
+function handleDragEnter (event) {
   event.preventDefault();
-  element.style.left = touch.pageX + offsetX + 'px';
-  element.style.top = touch.pageY + offsetY + 'px';
-}, false);
-
-
-var dragSrcEl = null;
-function handleDragStart(e) {
-  dragSrcEl = e.target;
-  var dt = e.dataTransfer;
-  dt.effectAllowed = 'move';
-  dt.setData('text', 'Bozs dragged');
-
-}
-function handleDragOver(e) {
-  if (dragSrcEl) {
-    e.preventDefault();
-    e.dataTransfer.dropEffect = 'move';
-
-  }
-}
-function handleDragEnter(e) {
-  if (dragSrcEl) {
-    e.target.classList.add('over');
-  }
-}
-function handleDragLeave(e) {
-  if (dragSrcEl) {
-    e.target.classList.remove('over');
-  }
-}
-function handleDragEnd(e) {
-  dragSrcEl = null;
-  /* [].forEach.call(cols, function (col) {
-      col.style.opacity = '';
-      col.classList.remove('over');
-  }); */
-}
-function handleDrop(e) {
-  if (dragSrcEl) {
-    e.stopPropagation();
-    e.stopImmediatePropagation();
-    e.preventDefault();
-    if (dragSrcEl != this) {
-      this.innerHTML = e.dataTransfer.getData('text');
-    }
-  }
+  event.target.classList.add('enterDropZone');
 }
 
-function dropElement(event) {
+function handleDragLeave (event) {
   event.preventDefault();
-  alert('dropping element');
+  event.target.classList.remove('enterDropZone');
+}
+
+function handleDrop (event) {
+  event.preventDefault();
+  console.log(event.dataTransfer.getData('text/plain'));
+  alert(event.dataTransfer.getData('text/plain'));
 }
 
