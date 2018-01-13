75cd0a2a356abd78bdebfc2f4f836567e2080fe5
Alexandre LANDREAU <alexandre@landreau.biz>
2018-01-13 14:53:06 +0100

    screen Ref

--- public/js/drag-n-drop.js
+++ public/js/drag-n-drop.js
@@ -18,9 +18,9 @@ for (var i=0; i<dropZones.length; i++) {
 function handleDragStart (event) {
   
   var el = (event.targetTouches)? event.targetTouches[0] : event;
-  console.log(posElement.x, window.offsetLeft, container.offsetLeft)
-  posElement.x = window.offsetLeft + container.offsetLeft;
-  posElement.y = window.offsetTop + container.offsetTop;
+  console.log(posElement.x, window.screenX, window.screenY, container.offsetLeft)
+  posElement.x = window.screenX + container.offsetLeft;
+  posElement.y = window.screenY + container.offsetTop;
 
   offsetX= element.offsetLeft - el.pageX;
   offsetY= element.offsetTop - el.pageY;
